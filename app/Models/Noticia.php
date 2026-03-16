49452b49edee6ab0b4b5de59b98b64bc7f98067c
IanNiu05 <yaelgutchi@gmail.com>
2026-03-16 16:15:52 -0600

    cambioC

--- app/Models/Noticia.php
+++ app/Models/Noticia.php
@@ -9,10 +9,8 @@ class Noticia extends Model
 {
     use HasFactory;
 
-    // Indicamos el nombre exacto de la tabla en HeidiSQL
     protected $table = 'noticias';
 
-    // Si en tu tabla de Heidi NO creaste las columnas created_at y updated_at,
-    // pon esta línea en false para que no te marque error:
+ 
     public $timestamps = true; 
 }
