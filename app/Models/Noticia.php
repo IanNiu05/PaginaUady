3455a54c57b18b192ed4ed738e6aec98e198e637
Isabelladx <isabelladurancpda@gmail.com>
2026-03-16 21:08:19 -0600

    Cambios en el archivo .env

--- app/Models/Noticia.php
+++ app/Models/Noticia.php
@@ -10,7 +10,5 @@ class Noticia extends Model
     use HasFactory;
 
     protected $table = 'noticias';
-
- 
     public $timestamps = true; 
 }
--- resources/views/layouts/inicio.blade.php
+++ resources/views/layouts/inicio.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
 
     <style>
-        /* --- ESTILOS GLOBALÉES --- */
+        /* --- ESTILOS GLOBALES --- */
         :root {
             --azul-uady: #1b365d;
             --dorado-uady: #cda043;
